6bba236c8da276e5725e81aa0daae3fe34bde0ec
Pratyush Kumar Singh <38942@ITCINFOTECH.COM>
2021-04-24 12:15:26 +0530

    Adding Board.js to for 3*3 matrix

---
+++ src/components/Board.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Square from './Square'
+const Board = ({ squares, onClick }) => (
+    <div className="board">
+      {squares.map((squares,i) => (
+                <Square key={i} value={squares} onClick={() => onClick(i)}/>
+                ))}
+                </div>
+  )
+export default Board
